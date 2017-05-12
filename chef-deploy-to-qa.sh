d1748ba7e655e219e2925348e1ca41742006c425
DeepakYadav17 <deepakpandhari.y@hcl.com>
2017-05-12 16:05:57 +0530

    Update chef-deploy-to-qa.sh

--- chef-deploy-to-qa.sh
+++ chef-deploy-to-qa.sh
@@ -1,1 +1,6 @@
 echo "Deploying to QA environment"
+export Username=$1
+export Password=$2
+export Story_Number=$3
+export Build_Version_Number=$4
+curl -i -X POST -u ${Username}:${Password} -H "Content-Type:text/xml" https://www51.v1host.com/HCLTechnologies72/rest-1.v1/Data/Story/${Story_Number} -d '<Asset><Attribute name="Custom_BuildNumberDeployed" act="set">'${Build_Version_Number}'</Attribute><Attribute name="Custom_DeployedEnvironment" act="set">QA</Attribute></Asset>'
