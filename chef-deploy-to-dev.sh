4d808a8076f48a6d5696ae7b6f257bce88ef407e
DeepakYadav17 <deepakpandhari.y@hcl.com>
2017-05-12 15:35:18 +0530

    Update chef-deploy-to-dev.sh

--- chef-deploy-to-dev.sh
+++ chef-deploy-to-dev.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 echo "Deploying to DEV region"
+export Username=$1
+export Password=$2
+export Story_Number=$3
+export Build_Version_Number=$4
+curl -i -X POST -u ${Username}:${Password} -H "Content-Type:text/xml" https://www51.v1host.com/HCLTechnologies72/rest-1.v1/Data/Story/${Story_Number} -d '<Asset><Attribute name="Custom_BuildNumberDeployed" act="set">'${Build_Version_Number}'</Attribute><Attribute name="Custom_DeployedEnvironment" act="set">DEV</Attribute></Asset>'
